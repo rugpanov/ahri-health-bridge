082b36aa56a2c1386a07857253786bdbe83e3b47
rugpanov <gripanov@gmail.com>
2026-05-19 00:59:18 +0100

    feat: parse steps JSON and wire store into controller

--- controllers/steps.go
+++ controllers/steps.go
@@ -1,19 +1,40 @@
 package controllers
 
-// StepLogger is the logging interface required by the steps controller.
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/rugpanov/ahri-health-bridge/utils"
+)
+
 type StepLogger interface {
 	Log(source string, body []byte)
 }
 
+type StepStore interface {
+	StoreSteps(ctx context.Context, steps int) error
+}
+
 type StepsController struct {
 	logger StepLogger
+	store  StepStore
 }
 
-func NewStepsController(logger StepLogger) *StepsController {
-	return &StepsController{logger: logger}
+func NewStepsController(logger StepLogger, store StepStore) *StepsController {
+	return &StepsController{logger: logger, store: store}
 }
 
-func (c *StepsController) Handle(body []byte) error {
+func (c *StepsController) Handle(ctx context.Context, body []byte) error {
+	var payload struct {
+		Steps *int `json:"steps"`
+	}
+	if err := json.Unmarshal(body, &payload); err != nil {
+		return fmt.Errorf("%w: invalid JSON", utils.ErrBadRequest)
+	}
+	if payload.Steps == nil {
+		return fmt.Errorf("%w: missing steps field", utils.ErrBadRequest)
+	}
 	c.logger.Log("steps", body)
-	return nil
+	return c.store.StoreSteps(ctx, *payload.Steps)
 }
